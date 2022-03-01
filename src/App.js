15c1ab445aa42011675a3a9aab5013dbb9289760
Justin Foell <630830+jrfoell@users.noreply.github.com>
2022-02-28 21:19:24 -0600

    Change title & logo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className='App-title'>Solunar API</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <footer className="App-footer">
+        <p>Solunar data provided by <a href="https://solunar.org/">solunar.org</a></p>
+      </footer>
     </div>
   );
 }
