26f1ca108d8317187296de28ae4de8be5750a0a4
Justin Foell <630830+jrfoell@users.noreply.github.com>
2022-03-11 21:58:23 -0600

    Add input.json

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import SolunarData from './components/SolunarData';
+import input from './input.json';
 
 function App() {
+  console.log('input: ', input);
   return (
     <div className="App">
       <header className="App-header">
         <h1 className='App-title'>Solunar API</h1>
-        <SolunarData />
+        <SolunarData lat={input.lat} lon={input.lon} date={input.date} utcOffset={input.utcOffset} />
       </header>
       <footer className="App-footer">
         <p>Solunar data provided by <a href="https://solunar.org/">solunar.org</a></p>
